54c34a44b629b1c5b888531cbe116fd63fb42b33
Matthias Robert <matthias.rbt120@gmail.com>
2020-07-25 11:46:37 +0200

    connexion bdd

--- v_destockage.php
+++ v_destockage.php
@@ -1,11 +1,20 @@
+<?php
+    include("./config/config.php");
+?>
 <!doctype html>
 <html>
     <head>
     <meta charset="UTF-8">
-    <title>destockage</title>
+    <title>Recherche</title>
     </head>
   <body>
   </body>
-  <h1>Prise de référence dans le stock</h1>
+<nav>
+  <ul><!-- Nave barre -->
+    <li><a  href="../HTML/Recherche.html">Rechercher référence</a></li><!-- Current link pour mettre en blanc la page actuelle -->
+    <li ><a id="currentLink2"  href="../HTML/Stockage.html">Stocker référence</a></li>
+  </ul>
+</nav> 
+  <h1>Stocker une référence</h1>
   <p>Test   1 </p>
 </html>
