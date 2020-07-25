58d21e6379753c544d19db746b537080be5c3e66
EtienneDouillard <edouillard@outlook.com>
2020-07-25 09:48:37 +0200

    test1

---
+++ v_destockage.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>destockage</title>
+    </head>
+  <body>
+  </body>
+  <h1>Prise de référence dans le stock</h1>
+  <p>Test   1 </p>
+</html>
---
+++ v_recherche.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>recherche</title>
+    </head>
+  <body>
+  </body>
+  <h1>Recherche de référence dans le stock</h1>
+  <p>Test   1 </p>
+</html>
